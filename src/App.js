2e4246ebb07e60bb72caaaef405655b781b55671
amij <amithjckm@gmail.com>
2022-08-14 14:45:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message,setMessage] = useState("");
+
+  // useEffect(()=>{
+  //  fetch("https://deploy-springboot-1.herokuapp.com/home/hello",{
+  //     method: 'GET',
+  //     mode: 'cors',
+  //     cache: 'no-cache',
+  //   }).then(response => response.text())
+  //   .then(res => {
+  //     setMessage(res)
+  //   })
+    
+
+  // },[])
+
+  function handleClick(){
+    
+    fetch("https://deploy-springboot-1.herokuapp.com/home/hello",{
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+    }).then(response => response.text())
+    .then(res => {
+      setMessage(res)
+    })
+    
+
+  }
+  function handleClearClick(){
+    setMessage("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to deploy app</h1>
+      <button onClick={handleClick}>
+        Test
+      </button>
+      <button onClick={handleClearClick}>
+       Clear
+      </button>
+      <h1>{message}</h1>
+      
     </div>
   );
 }
+
+
 
 export default App;
